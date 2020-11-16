e4552f3b9d19aa2fcd5df50e8a0ec834f853df32
Ashita <ashita9820@gmail.com>
2020-11-16 20:14:55 +0400

    added lesson info

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessons = [
+    {
+        "id": 1,
+        "subject":"Math",
+        "location":"Dubai",
+        "price":120,
+        "image":"../Images/math.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 2,
+        "subject":"English",
+        "location":"Abu Dhabi",
+        "price":100,
+        "image":"../Images/english.png",
+        "availableSpaces": 5
+    },
+    {
+        "id": 3,
+        "subject":"History",
+        "location":"Sharjah",
+        "price":85,
+        "image":"../Images/history.png",
+        "availableSpaces": 5
+    },
+    {
+        "id": 4,
+        "subject":"Geography",
+        "location":"Ajman",
+        "price":90,
+        "image":"../Images/geography.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 5,
+        "subject":"Information Technology",
+        "location":"Al Ain",
+        "price":110,
+        "image":"../Images/IT.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 6,
+        "subject":"Music",
+        "location":"Abu Dhabi",
+        "price":220,
+        "image":"../Images/music.png",
+        "availableSpaces": 5
+    },
+    {
+        "id": 7,
+        "subject":"Dance",
+        "location":"Dubai",
+        "price":250,
+        "image":"../Images/dance.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 8,
+        "subject":"Art",
+        "location":"Sharjah",
+        "price":170,
+        "image":"../Images/art.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 9,
+        "subject":"Business",
+        "location":"Ajman",
+        "price":150,
+        "image":"../Images/business.jpg",
+        "availableSpaces": 5
+    },
+    {
+        "id": 10,
+        "subject":"Psychology",
+        "location":"Dubai",
+        "price":180,
+        "image":"../Images/psych.jpg",
+        "availableSpaces": 5
+    }
+]
